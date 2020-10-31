f343e851c66cd8d011ba24cc9498e365130521ef
kavurisree <73729257+kavurisree@users.noreply.github.com>
2020-10-31 15:47:09 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,18 +1,17 @@
 unit=int(input("Enter the unit consumed: "))
-def Bill_Calc1(unit):          #function definition
-    if((unit>=1)and(unit<=50)):#between 1 - 50 units
+def Bill_Calc(unit):         
+    if((unit>=1)and(unit<=50)):
         print(unit*3);
     
-    elif((unit>50)and(unit<=100)):#between 50 - 100 units
-    
+    elif((unit>50)and(unit<=100)):
         print((50*3)+(unit-50)*6)
     
-    elif((unit>100)and(unit<=150)):#between 100 -  150 units
+    elif((unit>100)and(unit<=150)):
         print((50*3)+((100-50)*6)+(unit-100)*9)
     
-    elif(unit>200):           #above 200 units
+    elif(unit>200):          
         print((50*3)+((100-50)*6)+((150-100)*9)+(unit-150)*12)
     else:
             print("No usage ");
         #amount=0;
-Bill_Calc1(unit)             
+Bill_Calc(unit)             
